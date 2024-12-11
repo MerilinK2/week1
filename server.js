43a4c2b0314a5eaa34b2179a2cc697eddb15bb0f
Merilin. <merilin.kaselaid@tktk.ee>
2024-12-11 18:58:27 +0200

    week1 code

---
+++ server.js
@@ -0,0 +1,7 @@
+let firstName = 'Harry';
+let lastName = 'Potter';
+//templete literals
+//console.log (`Hello, ${firstName}!`);
+
+let greeting = `Hello, ${firstName} ${lastName}!`;
+console.log (greeting);
